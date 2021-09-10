0f13d2f53dcda8cdff82acac67b469375e1357fe
iproman <itechman@yandex.ru>
2021-09-10 20:50:39 +0500

    Add create function

--- app/components/create/create.component.js
+++ app/components/create/create.component.js
@@ -2,6 +2,31 @@
 
 angular.module('myApp.create')
 
-    .controller('CreateCtrl', [function () {
+    .controller('CreateCtrl', function ($scope, $routeParams, $http, $route) {
+        var self = $scope;
+        self.disabled = false;
 
-    }]);
+        self.master = {};
+
+        self.submit = function (user) {
+            self.disabled = true;
+
+            $http.post('https://jsonplaceholder.typicode.com/users/', user)
+                .then(function (res) {
+                    self.successMsg = res;
+                })
+                .catch(function (err) {
+                    self.errorMsg = err;
+                })
+                .finally(function () {
+                    self.disabled = false;
+                })
+            ;
+        };
+
+        self.reset = function () {
+            self.user = {};
+        };
+
+        self.reset();
+    });
