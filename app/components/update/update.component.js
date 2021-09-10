3b5d6016a8583a7baddf1fa98cc2d8d26bf0d0fc
iproman <itechman@yandex.ru>
2021-09-10 21:00:54 +0500

    Fill user with up data

--- app/components/update/update.component.js
+++ app/components/update/update.component.js
@@ -2,6 +2,41 @@
 
 angular.module('myApp.update')
 
-    .controller('UpdateCtrl', [function () {
+    .controller('UpdateCtrl', function ($scope, $routeParams, $http, $route) {
+        var self = $scope;
+        self.disabled = false;
+        self.id = $routeParams.id;
 
-    }]);
+        self.user = {};
+
+        $http.get('https://jsonplaceholder.typicode.com/users/' + self.id)
+            .then(function (res) {
+               self.user = res.data;
+            })
+            .catch(function (err) {
+                self.errorMsg = err;
+            })
+        ;
+
+        self.submit = function (user) {
+            self.disabled = true;
+
+            $http.put('https://jsonplaceholder.typicode.com/users/' + self.id, user)
+                .then(function (res) {
+                    self.successMsg = res;
+                })
+                .catch(function (err) {
+                    self.errorMsg = err;
+                })
+                .finally(function () {
+                    self.disabled = false;
+                })
+            ;
+        };
+
+        self.reset = function () {
+            self.user = {};
+        };
+
+        self.reset();
+    });
