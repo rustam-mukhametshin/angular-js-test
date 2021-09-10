d63e5335b787495cf125b4d0d241c3a9615d0847
iproman <itechman@yandex.ru>
2021-09-10 20:33:40 +0500

    Show user data

--- app/components/read/read.component.js
+++ app/components/read/read.component.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('myApp.read')
-    .controller('ReadCtrl', [function () {
+    .controller('ReadCtrl', function ($scope, $routeParams, $http) {
+        var self = $scope;
+        var id = $routeParams.id;
 
-    }]);
+        $http.get('https://jsonplaceholder.typicode.com/users/' + id).then(function (res) {
+            self.user = res.data;
+        });
+    });
