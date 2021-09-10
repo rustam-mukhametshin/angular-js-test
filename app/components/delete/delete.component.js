8c915a9ae6bb10fd76e79d0effddd86e076f5e1f
iproman <itechman@yandex.ru>
2021-09-10 21:17:16 +0500

    Add delete function

--- app/components/delete/delete.component.js
+++ app/components/delete/delete.component.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('myApp.delete')
-    .controller('DeleteCtrl', [function ($routeParams) {
+    .controller('DeleteCtrl', function ($scope, $routeParams, $http) {
+        var self = $scope;
+        self.id = $routeParams.id;
+        self.successMsg = '';
 
-    }]);
+        $http.delete('https://jsonplaceholder.typicode.com/users/' + self.id).then(function (res) {
+            self.successMsg = res;
+        });
+    });
