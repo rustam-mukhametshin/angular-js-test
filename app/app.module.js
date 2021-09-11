09b980883a9efef2b7e182c121eb194b277d7f82
iproman <itechman@yandex.ru>
2021-09-11 19:26:53 +0500

    Fix error

--- app/app.module.js
+++ app/app.module.js
@@ -3,7 +3,6 @@
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
     'ngRoute',
-    'myApp.view1',
     'myApp.home',
     'myApp.update',
     'myApp.delete',
