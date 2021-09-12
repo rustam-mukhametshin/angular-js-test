14048bf8542c05870c74309d75579345b16f0095
iproman <itechman@yandex.ru>
2021-09-12 20:25:12 +0500

    Not copy scss file

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
             main: {
                 expand: true,
                 cwd: 'app/',
-                src: ['**', '!js/**', '!lib/**', '!**/*.css'],
+                src: ['**', '!js/**', '!lib/**', '!**/*.css', '!**/*.scss'],
                 dest: 'dist/'
             },
             shims: {
