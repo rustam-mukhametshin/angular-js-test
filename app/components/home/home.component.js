4139aa06c1c570254664e790fb74b13e4eecc047
iproman <itechman@yandex.ru>
2021-09-12 20:05:56 +0500

    Add function for pagination

--- app/components/home/home.component.js
+++ app/components/home/home.component.js
@@ -5,13 +5,38 @@ angular.module('myApp.home')
     .controller('HomeCtrl', function ($scope, $http) {
         var self = $scope;
 
-        self.thead = [
-            '#',
-            'Username',
-            'Email',
-            'Phone',
-        ];
         $http.get('https://jsonplaceholder.typicode.com/users').then(function (res) {
             self.users = res.data;
+
+            self.currentPage = 1;
+            self.itemsPerPage = 2;
+
+            self.thead = [
+                '#',
+                'Username',
+                'Email',
+                'Phone',
+            ];
+
+            self.beforeDisable = function () {
+                return self.currentPage === 1;
+            };
+            self.nextDisable = function () {
+                return self.currentPage === self.totalPages;
+            };
+
+            self.before = function () {
+                self.currentPage--;
+            };
+
+            self.next = function () {
+                self.currentPage++;
+            };
+
+
+            self.totalPages = self.users.length / self.itemsPerPage;
+
+            self.beforeDisable();
+            self.nextDisable();
         });
     });
